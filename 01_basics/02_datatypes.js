961b5d6a365e6d6d4966716ca5f8388932db38f2
Utkarsh Singh <84698112+UtkarshSingh-gitH@users.noreply.github.com>
2026-02-02 07:18:32 +0000

    datarypes_part1

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat whole file as newer version of JS //tc39.cs
+/*
+number => 2^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined=> no value yet assigned but is epected
+symbol => uniqueness of componenet in react figma etc
+-----------------------------------------------------
+object
+*/
+let name = "utakrsh"
+let age = 24
+let isLoggedIn = false
+let state;
+
+console.log(typeof "hitesh")
+//typeof of null is object and for undefined it is undefined
