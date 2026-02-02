6551951eed18765e8d325474ff1070143690f8e1
Utkarsh Singh <84698112+UtkarshSingh-gitH@users.noreply.github.com>
2026-02-02 06:54:57 +0000

    declaring variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 144253
+let accountMail = "some@gmail.com"
+var accountPass = "12345"
+accountCity = "Pune"
+
+console.table([accountID, accountMail, accountPass, accountCity])
+
+//var used to have problem of scope that got resolved in let hence we only use let now
