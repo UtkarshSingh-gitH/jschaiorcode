09ebde009193223b62616b2ecffcc23a1713384b
Utkarsh Singh <84698112+UtkarshSingh-gitH@users.noreply.github.com>
2026-02-03 09:28:26 +0000

    basic conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score1 = 33
+let score2 = "33abc"
+
+let num1 = Number(score1)
+let num2 = Number(score2)
+console.log(typeof(score1))
+console.log(typeof(score2))
+console.log(typeof(num1))
+console.log(typeof(num2))
+console.log(num2)
+
+/*
+33 => 33
+"33abc" => Nan
+true => 1 ; false => 0
+anyvalue => true ; zero => 0
+*/
+
