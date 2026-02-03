3f675e411d6210563dda0010a6483e22321684a1
Utkarsh Singh <84698112+UtkarshSingh-gitH@users.noreply.github.com>
2026-02-03 12:13:52 +0000

    some modifications made

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -17,3 +17,16 @@ let state;
 
 console.log(typeof "hitesh")
 //typeof of null is object and for undefined it is undefined
+
+//Primitive :: all types that are call by value which means a copy is called and chnaged are made there.
+
+// they are => String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Refernce type :: they are called as a reference
+
+// => Array, Objects, Functions
+
+let myObj = {
+    name : "utkarsh",
+    age: 24
+}
