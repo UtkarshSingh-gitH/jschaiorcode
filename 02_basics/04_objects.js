6fecf1b07a9b9c27151eaf556c8e5c0963be4383
Utkarsh Singh <84698112+UtkarshSingh-gitH@users.noreply.github.com>
2026-02-05 11:46:34 +0000

    basics of API

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -15,3 +15,20 @@ console.log(obj3)
 console.log(Object.keys(user)) //this returns an ARRAY as result with all keys in it.
 console.log(Object.entries(user)) //this returns an ARRAY as result with all key:value pair in it.
 console.log(user.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    name: "js",
+    price: "100",
+    courseInstructor: "hitesh"
+
+}
+
+const {courseInstructor: Instructor} = course //this extracts the value
+console.log(Instructor)
+
+const navbar = ({comapny}) => {}
+navbar(comapny="hitesh")
+
+//api is more like just values that we get from backend  and how we write it 
+//object hi JSON hai, but both sides under ""
+
